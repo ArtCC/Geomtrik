643d046fc3562fad4bf321bbea5378b14ca59818
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2021-04-06 13:53:01 +0200

    Add figures.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
     public float velocity;
 
-    private Rigidbody2D rbody;
+    private Rigidbody2D rigid;
     private float degreesPerSec = 360f;
+    private float horizontal;
+    private Vector2 lastTouchPosition;
 
     // Start is called before the first frame update
     void Start() {
-        rbody = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            rbody.velocity = Vector2.up * velocity;
+            rigid.velocity = Vector2.up * velocity;
             float rotAmount = degreesPerSec * Time.deltaTime;
             float curRot = transform.localRotation.eulerAngles.z;
             transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot+rotAmount));
         }
+        lateralMovement();
+    }
+
+    // Private functions
+    private void lateralMovement() {
+        // With tap screen in mobile
+        if (Input.touchCount > 0) {
+                lastTouchPosition = Input.touches[0].position;
+                float width = Screen.width;                
+                if (lastTouchPosition.x < (width / 2 + 50)) {
+                    horizontal = -1.0f;
+                } else {
+                    horizontal = 1.0f;
+                }
+                rigid.velocity = new Vector2(horizontal * 2, rigid.velocity.y);
+            }
     }
 }
