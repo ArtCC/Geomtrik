135510cd5fbfa304b518cfdf21b8abf901ad2e9d
Arturo Carretero Calvo <a.carretero.calvo@accenture.com>
2021-04-06 16:16:47 +0200

    Add prefabs. Add physics for prefabs. Add collider in obstacle for player win.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour {
     private float degreesPerSec = 360f;
     private float horizontal;
     private Vector2 lastTouchPosition;
+    private bool onPosition = false;
+    private bool onPlayerWin = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -26,20 +28,45 @@ public class Player : MonoBehaviour {
             transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot+rotAmount));
         }
         lateralMovement();
+        playerWin();
     }
 
     // Private functions
     private void lateralMovement() {
         // With tap screen in mobile
         if (Input.touchCount > 0) {
-                lastTouchPosition = Input.touches[0].position;
-                float width = Screen.width;                
-                if (lastTouchPosition.x < (width / 2 + 50)) {
-                    horizontal = -1.0f;
-                } else {
-                    horizontal = 1.0f;
-                }
-                rigid.velocity = new Vector2(horizontal * 2, rigid.velocity.y);
+            lastTouchPosition = Input.touches[0].position;
+            float width = Screen.width;                
+            if (lastTouchPosition.x < (width / 2 + 50)) {
+                horizontal = -1.0f;
+            } else {
+                horizontal = 1.0f;
             }
+            rigid.velocity = new Vector2(horizontal * 2, rigid.velocity.y);
+        }
+    }
+
+    private void playerWin() {
+        if (onPosition == true && onPlayerWin == true && rigid.velocity.x == 0 && rigid.velocity.y == 0) {
+            onPlayerWin = true;
+            Debug.Log("You win!!!");
+        }
+    }
+
+    private void reloadGame() {
+        onPosition = false;
+        onPlayerWin = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider) {
+        if (collider.gameObject.tag == "Obstacle") {
+            onPosition = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collider) {
+        if (collider.gameObject.tag == "Obstacle") {
+            onPosition = false;
+        }
     }
 }
